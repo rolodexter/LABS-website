1d6398640af0ed1a2560d403105c3b8c3bfce83b
rolodexter <jmaristela2023@gmail.com>
2025-03-25 06:35:58 +0800

    Add black-and-white teaser landing page

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Serve static files from the current directory
+app.use(express.static(__dirname));
+
+// For any request, send the index.html file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`rolodexterLABS website is running on port ${PORT}`);
+});
